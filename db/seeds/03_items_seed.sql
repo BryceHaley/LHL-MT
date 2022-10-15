1bdff60bd31a48a280a73748ac1e747631997079
Bryce Haley <bhaley91@gmail.com>
2022-10-15 08:47:41 -0400

    seed items

---
+++ db/seeds/03_items_seed.sql
@@ -0,0 +1,5 @@
+-- items Table seeds here
+
+INSERT INTO items (name, description, price) VALUES ('coke', 'warm and flat', 1.25);
+INSERT INTO items (name, description, price) VALUES ('cake', 'cold and soggy', 420.69);
+INSERT INTO items (name, description, price) VALUES ('koala', 'perfectly roasted', 5.00);
