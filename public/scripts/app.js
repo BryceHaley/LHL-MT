eb255e8b3d18623ff523a2d629f0b48f1656aa98
Jaskarn <jaskarn15@gmail.com>
2022-10-19 21:07:44 +0000

    add app.js scripts

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,50 @@
 // Client facing scripts here
+
+
+//Create individual items
+$(document).ready(() => {
+
+
+  const loadItems = function() {
+    $.ajax({
+      method: "GET",
+      url: "/api/items"
+    })
+      .then((data) => {
+        renderItems(data.items);
+      });
+  };
+
+
+  const createItemElement = function(menuItems) {
+    let $itemContainer = $(`
+    <article class="dish-content">
+      <img src="/images/cakes.jpg">
+
+      <div class="menu-column">
+        <div class="dish-price">
+          <h2 class="dish-title">${menuItems.name}</h2>
+          <h2>${menuItems.price}</h2>
+       </div>
+       <div class="dish-description">
+          <h4>${menuItems.description}</h4>
+        </div>
+        <div class="dish-buttons">
+          <button class="add-button" type="submit">ADD TO CART</button>
+        </div>
+      </div>
+    </article>`);
+    return $itemContainer;
+  };
+
+
+  const renderItems = function(items) {
+    for (const item of items) {
+      const $item = createItemElement(item);
+      $(".menu-sections").append($item);
+    }
+  }
+
+  loadItems();
+
+})
