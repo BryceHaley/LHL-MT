782b60ef52b05950792bda7d8b1b39e59d867363
Bryce Haley <bhaley91@gmail.com>
2022-10-20 15:44:16 -0400

    load and render orders functionality

--- public/scripts/orders.js
+++ public/scripts/orders.js
@@ -2,8 +2,36 @@ const loadOrders = function() {
   $.get("/api/orders", (data, status) => {
     console.log(`data: ${JSON.stringify(data.items)}`);
     console.log(`status: ${status}`);
+    renderOrders(data.items)
   });
-}
+};
+
+const createOrderElement = function(order) {
+  let $orderContainer = $(`
+  <article class="order-content">
+    <div class="menu-column">
+      <div class="dish-price">
+        <h2 class="dish-title">${order.order_id}</h2>
+        <h2>${order.order_status}</h2>
+     </div>
+     <div class="dish-description">
+        <h4>${order.total_cost}</h4>
+        <h4>${order.status}</h4>
+        <h4>${order.order_time}</h4>
+        <h4>${order.projected_completion}</h4>
+      </div>
+    </div>
+  </article>`);
+
+  return $orderContainer;
+};
+
+const renderOrders = function(orders) {
+  for (const order of orders) {
+    const $order = createOrderElement(order);
+    $(".order-sections").append($order);
+  }
+};
 
 $(document).ready(() => {
   loadOrders();
